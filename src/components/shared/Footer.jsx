1ebfdac763cae0132754792a64f37dce6d321347
komgridy <komgridy@gmail.com>
2023-08-06 13:03:41 +0700

    update deploy

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -49,20 +49,38 @@ function Footer() {
                 <h5>Subscribe to our newsletter</h5>
                 <p>Monthly digest of whats new and exciting from us.</p>
                 <div className="d-flex w-100 gap-2">
-                  <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
-                  <button className="btn btn-primary" type="button">Subscribe</button>
+                  <input
+                    id="newsletter1"
+                    type="text"
+                    className="form-control"
+                    placeholder="Email address"
+                  />
+                  <button className="btn btn-primary" type="button">
+                    Subscribe
+                  </button>
                 </div>
               </form>
             </div>
           </div>
-          <div className="d-flex justify-content-between py-4 my-4 border-top">
+          <div classname="d-flex justify-content-between py-4 my-4 border-top">
             <p>© 2023 Company, Inc. All rights reserved.</p>
-            <ul className="list-unstyled d-flex">
-              <li className="ms-3"><svg className="bi" width={24} height={24}><use xlinkHref="#twitter" /></svg></li>
-              <li className="ms-3"><svg className="bi" width={24} height={24}><use xlinkHref="#instagram" /></svg></li>
-              <li className="ms-3"><svg className="bi" width={24} height={24}><use xlinkHref="#facebook" /></svg></li>
+            <ul classname="list-unstyled d-flex">
+              <li classname="ms-3">
+                <svg classname="bi" width="{24}" height="{24}">
+                  <use xlinkhref="#twitter" />
+                </svg>
+              </li>
+              <li classname="ms-3">
+                <svg classname="bi" width="{24}" height="{24}">
+                  <use xlinkhref="#instagram" />
+                </svg>
+              </li>
+              <li classname="ms-3">
+                <svg classname="bi" width="{24}" height="{24}">
+                  <use xlinkhref="#facebook" />
+                </svg>
+              </li>
             </ul>
-
           </div>
         </footer>
       </div>
